67a43861929ff78b79ff9e3b12e78040f5355cb3
Bert van Lange <bvanlange@student.tudelft.nl>
2025-11-10 21:09:28 +0100

    Remove target directory from git

--- punten_telling_tik_spel/target/release/build/serde-9e59eb4de4feaddb/build_script_build-9e59eb4de4feaddb.d
+++
@@ -1,5 +0,0 @@
-/root/punten_telling_tik_spel/punten_telling_tik_spel/target/release/build/serde-9e59eb4de4feaddb/build_script_build-9e59eb4de4feaddb.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/root/punten_telling_tik_spel/punten_telling_tik_spel/target/release/build/serde-9e59eb4de4feaddb/build_script_build-9e59eb4de4feaddb: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:
